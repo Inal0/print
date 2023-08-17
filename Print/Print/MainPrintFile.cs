0a9534a009a8e30434b0fd5240043a4d1b52373e
Inal0 <138590266+Inal0@users.noreply.github.com>
2023-08-17 10:45:58 +0400

    Add files via upload

---
+++ Print/Print/MainPrintFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print
+{
+    internal class MainPrintFile
+    {
+        public void Print(object file)
+        {
+            if (file is Word)
+            {
+                Console.WriteLine("word cap edildi");
+
+            }
+            if (file is Excel)
+            {
+                Console.WriteLine("excel cap edildi");
+            }
+            if (file is PDF)
+            {
+                Console.WriteLine("excel cap edildi");
+            }
+        }
+    }
+}
---
+++ Print/Print/Program.cs
@@ -0,0 +1,8 @@
+﻿using Print;
+
+MainPrintFile print=new MainPrintFile();
+Word word=new Word();
+Excel excel=new Excel();
+PDF pdf=new PDF();
+
+print.Print(excel);
